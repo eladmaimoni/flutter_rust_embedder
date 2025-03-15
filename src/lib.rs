38ad2fe103ca6c35d782ebf7405e4b30cbc1d6ef
Elad Maimoni <mr.maimon@gmail.com>
2025-03-15 16:11:44 +0200

    simple window creation example

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,39 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, EventLoop};
+use winit::window::{Window, WindowAttributes, WindowId};
+
 mod logging;
 mod windowing;
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Default, Debug)]
+struct App {
+    window: Option<Window>,
+    // window: Option<Box<dyn Window>>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window_attributes = WindowAttributes::default();
+        self.window = event_loop.create_window(window_attributes).ok();
+
+        todo!()
     }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        todo!()
+    }
+}
+
+pub fn run() -> Result<(), Box<dyn std::error::Error>> {
+    let event_loop = EventLoop::new()?;
+    let mut app = App::default();
+
+    // For alternative loop run options see `pump_events` and `run_on_demand` examples.
+    event_loop.run_app(&mut app).map_err(Into::into)
 }
